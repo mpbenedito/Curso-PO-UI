ac672c8f4a64658ce0166a689c6bffafaf5bb861
Marcelo Benedito <mpbenedito@gmail.com>
2025-01-09 20:16:47 -0300

    A navbar já formatada e componentes somente criados

---
+++ Curso/projAngular/projeto/src/app/paginas/clietes/clietes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clietes',
+  imports: [],
+  templateUrl: './clietes.component.html',
+  styleUrl: './clietes.component.css'
+})
+export class ClietesComponent {
+
+}
---
+++ Curso/projAngular/projeto/src/app/paginas/orcamentos/orcamentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamentos',
+  imports: [],
+  templateUrl: './orcamentos.component.html',
+  styleUrl: './orcamentos.component.css'
+})
+export class OrcamentosComponent {
+
+}

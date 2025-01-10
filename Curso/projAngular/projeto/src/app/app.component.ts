3ea717514c1f1ffa927e4983a4a16c81dbfb9307
Marcelo Benedito <mpbenedito@gmail.com>
2025-01-09 21:01:28 -0300

    Estilo dos componentes configurados.

--- Curso/projAngular/projeto/src/app/app.component.ts
+++ Curso/projAngular/projeto/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { ClientesComponent } from './paginas/clientes/clientes.component';
+import { OrcamentosComponent } from './paginas/orcamentos/orcamentos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ClientesComponent, OrcamentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

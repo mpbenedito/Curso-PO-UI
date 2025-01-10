38372ad768c9789f9ab997293aa9e8c3d4c339df
Marcelo Benedito <mpbenedito@gmail.com>
2025-01-09 21:40:53 -0300

    Rotas configuradas e página de erro

--- Curso/projAngular/projeto/src/app/app.routes.ts
+++ Curso/projAngular/projeto/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { ClientesComponent } from './paginas/clientes/clientes.component';
+import { OrcamentosComponent } from './paginas/orcamentos/orcamentos.component';
+import { ErroComponent } from './paginas/erro/erro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'orcamentos', component: OrcamentosComponent},
+  {path: '**', component: ErroComponent}
+];
---
+++ Curso/projAngular/projeto/src/app/paginas/erro/erro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-erro',
+  imports: [],
+  templateUrl: './erro.component.html',
+  styleUrl: './erro.component.css'
+})
+export class ErroComponent {
+
+}
